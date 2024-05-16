64328d972590c6e00126b4c0ecc06524e21e8c26
Thomas-Stoianov <thomasgstoianov2@gmail.com>
2024-05-16 17:19:51 -0300

    Feat: api feita, fazendo o formulario

--- script.js
+++ script.js
@@ -88,22 +88,7 @@ const selecionarSlide = (indiceSlide) => {
 
 
 let listaCases = [
-    {
-        imagem: "https://unsplash.it/600/400?image=40",
-        descricao: "Uma empresa de tecnologia lança um desafio de gamificação onde os funcionários devem propor e implementar ideias inovadoras"
-    },
-    {
-        imagem: "https://unsplash.it/600/400?image=51",
-        descricao: "Empresa de consultoria cria uma narrativa interativa de gamificação para seu programa de treinamento"
-    },
-    {
-        imagem: "https://unsplash.it/600/400?image=67",
-        descricao: "Uma empresa de vendas implementa uma competição gameficada entre equipes que competem pelo topo do ranking"
-    },
-    {
-        imagem: "https://unsplash.it/600/400?image=78",
-        descricao: "Uma empresa de saúde promove o bem-estar dos funcionários através de um desafio de gamificação de condicionamento"
-    }
+ 
 ]
 
 const renderizarCases = () => {
@@ -121,4 +106,46 @@ const renderizarCases = () => {
     })
 
     elementoLista.innerHTML = template
+}
+
+const carregarCases = () => {
+    fetch("http://localhost:3000/cases")
+    .then( resposta => resposta.json())
+    .then ( (dados) => {
+        listaCases = dados
+        renderizarCases()
+    } )
+}
+
+
+const solicitarOrcamento = () => {
+    // pegar valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-descricao").value
+
+    console.log(valorNome);
+    console.log(valorEmail);
+    console.log(valorDescricao);
+    // organizar objeto com os valores
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao,
+    }
+    // enviar requisicao para a api
+    // 127.0.0.1 -> localhost
+    // método HTTP POST - create -> cadastrar ou criar
+    fetch("http://127.0.0.1:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => console.log(resposta))
+    .catch(erro => console.error(erro))
+    // limpar os campos
+    // mostrar alert com msg de sucesso
+    // caso erro - alert com msg erro
 }
